94ad74fedb77810e8d0b03562b024e5749714694
JieiGarcia <jiei.aca@gmail.com>
2024-05-12 13:15:48 +0900

    meh

--- lib/main.dart
+++ lib/main.dart
@@ -6,4 +6,47 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Basic Widgets',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Basic Widgets Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Press me'),
+            ),
+            Image.network(
+                'https://flutter.dev/images/flutter-logo-sharing.png'),
+          ],
+        ),
+      ),
+    );
+  }
 }
